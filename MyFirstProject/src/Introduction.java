d7455eba8728214e4af538a08edb70498e29644f
Joe Gallotta <joeygallotta@gmail.com>
2019-11-26 10:02:01 -0800

    write a method that displays information

--- MyFirstProject/src/Introduction.java
+++ MyFirstProject/src/Introduction.java
@@ -1,6 +1,9 @@
 
 public class Introduction {
 public static void main (String[]args) {
-	System.out.println("testing, hi there!");
+	System.out.println("My name is Joey Gallotta.");
+	System.out.println("My favorite color is blue.");
+	System.out.println("If I won the lottery, I would use the money to help others.");
+	
 	}
 }
